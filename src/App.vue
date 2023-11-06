45ae5b2981d3c97aa2bfee0fa5a0974ada48e484
EmanueleVenditti95 <vendittiema@gmail.com>
2023-11-06 16:22:00 +0100

    Add - general style in sass file

--- src/App.vue
+++ src/App.vue
@@ -21,36 +21,6 @@ export default {
   <Main />
 </template>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-  font-family: 'Open Sans', sans-serif;
-}
-
-body {
-  position: relative;
-}
-
-ol,
-ul,
-li {
-  list-style: none;
-}
-
-.container {
-  width: 800px;
-  margin: 0 auto;
-}
-
-.row {
-  display: flex;
-  flex-direction: row;
-}
-
-img {
-  max-width: 100%;
-}
+<style lang="scss">
+@use './styles/general.scss'
 </style>
